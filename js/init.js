26948627ed17135ed6542a1f25203a2923682155
Thomas Wagner <thwa1@web.de>
2021-07-28 17:27:06 +0200

    details with dyn. age

--- js/init.js
+++ js/init.js
@@ -1,8 +1,18 @@
 (function ($) {
   $(function () {
-
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function pageLoaded() {
+  const birthDate = new Date(1995, 6, 4);
+  document.getElementById("age").innerHTML = calculateAge(birthDate);
+}
+
+function calculateAge(date) {
+  const now = new Date();
+  const diff = Math.abs(now - date);
+  const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+  return age;
+}
